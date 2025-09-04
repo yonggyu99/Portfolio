2c4ae25fd7145abe3c9f08f0e40ee1dc98535c05
박용규 <yoyo5411@naver.com>
2025-09-04 19:32:04 +0900

    feat : HomeSection 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,10 @@
 import MainLayout from '../layouts/MainLayout';
+import HomeSection from '../features/home/HomeSection';
 
 const Home = () => {
   return (
     <MainLayout>
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold text-white mb-4">
-            Welcome to My Portfolio
-          </h1>
-          <p className="text-gray-300 text-lg">Frontend Developer</p>
-        </div>
-      </div>
+      <HomeSection />
     </MainLayout>
   );
 };

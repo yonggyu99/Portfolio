b5f4c1d0809ea4b1ff2b76a2162881008d96ae8e
YongGyu <yoyo5411@naver.com>
2025-09-26 18:09:21 +0900

    feat : a tag style 수정

--- src/components/NotionRenderer.tsx
+++ src/components/NotionRenderer.tsx
@@ -240,8 +240,7 @@ const NotionRenderer = ({ pageId, className = '' }: NotionRendererProps) => {
             href={href}
             target="_blank"
             rel="noopener noreferrer"
-            className="text-blue-600 hover:text-blue-800
-  underline"
+            className="font-bold hover:underline"
           >
             {element}
           </a>

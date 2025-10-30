a11effc51781315fd6aaa2d515e1de514cd1c3f9
YongGyu <yoyo5411@naver.com>
2025-10-30 14:19:53 +0900

    chore : footer breakpoint  수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -42,11 +42,11 @@ const Footer = () => {
 
   return (
     <footer className="fixed bottom-0 left-0 right-0 z-50 hidden md:block">
-      {/* iPad: 우측 하단에 가로 배치 */}
+      {/* iPad: 우측 하단에 가로 배치 (768px ~ 1279px) */}
       <motion.div
         animate={{ opacity: isContactVisible ? 0 : 1 }}
         transition={{ duration: 0.5, ease: 'easeInOut' }}
-        className="absolute bottom-6 right-6 flex gap-4 lg:hidden"
+        className="absolute bottom-6 right-6 flex gap-4 xl:hidden"
       >
         <a
           href="https://github.com/yonggyu99"
@@ -65,12 +65,12 @@ const Footer = () => {
         </button>
       </motion.div>
 
-      {/* 노트북: 양쪽 배치 (크기 축소) */}
+      {/* 노트북: 양쪽 배치 (1280px 이상) */}
       {/* 왼쪽: GitHub + 세로라인 */}
       <motion.div
         animate={{ opacity: isContactVisible ? 0 : 1 }}
         transition={{ duration: 0.5, ease: 'easeInOut' }}
-        className="absolute bottom-0 left-6 hidden lg:flex flex-col items-center"
+        className="absolute bottom-0 left-6 hidden xl:flex flex-col items-center"
       >
         <a
           href="https://github.com/yonggyu99"
@@ -87,7 +87,7 @@ const Footer = () => {
       <motion.div
         animate={{ opacity: isContactVisible ? 0 : 1 }}
         transition={{ duration: 0.5, ease: 'easeInOut' }}
-        className="absolute bottom-0 right-6 hidden lg:flex flex-col items-center"
+        className="absolute bottom-0 right-6 hidden xl:flex flex-col items-center"
       >
         <button
           onClick={handleEmailCopy}
